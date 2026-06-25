c89d70c7ca9aacc18d6727e6d4c99e11bcdbf8d1
labrinyang <zhiweiy9125@gmail.com>
2026-06-26 00:34:14 +0800

    feat: add useBorrowEModeStatus hook

---
+++ packages/kit/src/views/Borrow/hooks/useBorrowEModeStatus.ts
@@ -0,0 +1,40 @@
+import backgroundApiProxy from '@onekeyhq/kit/src/background/instance/backgroundApiProxy';
+import { usePromiseResult } from '@onekeyhq/kit/src/hooks/usePromiseResult';
+
+interface IUseBorrowEModeStatusParams {
+  networkId?: string;
+  provider?: string;
+  marketAddress?: string;
+  accountId?: string;
+  enabled?: boolean;
+}
+
+export const useBorrowEModeStatus = ({
+  networkId,
+  provider,
+  marketAddress,
+  accountId,
+  enabled = true,
+}: IUseBorrowEModeStatusParams) => {
+  const {
+    result: eModeStatus,
+    run,
+    isLoading,
+  } = usePromiseResult(
+    async () => {
+      if (!networkId || !provider || !marketAddress || !accountId || !enabled) {
+        return null;
+      }
+      return backgroundApiProxy.serviceStaking.getBorrowEModeStatus({
+        networkId,
+        provider,
+        marketAddress,
+        accountId,
+      });
+    },
+    [networkId, provider, marketAddress, accountId, enabled],
+    { initResult: null, watchLoading: true, alwaysSetState: true },
+  );
+
+  return { eModeStatus, isLoading, refresh: run };
+};
